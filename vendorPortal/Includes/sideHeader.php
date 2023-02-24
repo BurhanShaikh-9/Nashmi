f0d8e5262162b17ba9eb71eba98275ce50f7f92b
Burhan Ali Shaikh <85751219+Nyx-95@users.noreply.github.com>
2023-02-24 16:43:13 +0500

    first commit

---
+++ vendorPortal/Includes/sideHeader.php
@@ -0,0 +1,78 @@
+<aside id="sidebar" class="sidebar">
+
+  <ul class="sidebar-nav" id="sidebar-nav">
+
+    <li class="nav-item">
+      <a class="nav-link navLinkJQ collapsed" href="index.php">
+        <i class="bi bi-grid"></i>
+        <span>Dashboard</span>
+      </a>
+    </li><!-- End Dashboard Nav -->
+
+    <li class="nav-item">
+      <a class="nav-link navLinkJQ collapsed" href="offer.php">
+        <i class="bi bi-tags"></i>
+        <span>Offers</span>
+      </a>
+    </li><!-- End Create Business Profile Nav -->
+
+    <li class="nav-item">
+      <a class="nav-link navLinkJQ collapsed" href="businessProfileCreate.php">
+        <i class="bi bi-briefcase"></i>
+        <span>Create Business Profile</span>
+      </a>
+    </li><!-- End Create Business Profile Nav -->
+
+    <li class="nav-item">
+      <a class="nav-link navLinkDdJQ collapsed" data-bs-target="#charts-nav" data-bs-toggle="collapse" href="#">
+      <i class="bi bi-calendar-check"></i><span>My Orders</span><i class="bi bi-chevron-down ms-auto"></i>
+      </a>
+      <ul id="charts-nav" class="nav-content navLinkUlList collapse " data-bs-parent="#sidebar-nav">
+        <li>
+          <a href="pastOrder.php" class="navLinkA ">
+            <i class="bi bi-circle"></i><span>Past Orders</span>
+          </a>
+        </li>
+        <li>
+          <a href="activeOrder.php" class="navLinkA ">
+            <i class="bi bi-circle"></i><span>Active Orders</span>
+          </a>
+        </li>
+
+      </ul>
+    </li><!-- End Reports Nav -->
+
+    <li class="nav-item">
+      <a class="nav-link navLinkDdJQ1 collapsed" data-bs-target="#charts-nav1" data-bs-toggle="collapse" href="#">
+      <i class="bi bi-house-heart"></i><span>Set Up Restaurant</span><i class="bi bi-chevron-down ms-auto"></i>
+      </a>
+      <ul id="charts-nav1" class="nav-content navLinkUlList1 collapse " data-bs-parent="#sidebar-nav">
+        <li>
+          <a href="restaurantCategories.php" class="navLinkB ">
+            <i class="bi bi-circle"></i><span>Categories</span>
+          </a>
+        </li>
+        <li>
+          <a href="restaurantListing.php" class="navLinkB ">
+            <i class="bi bi-circle"></i><span>Listings</span>
+          </a>
+        </li>
+
+      </ul>
+    </li><!-- End Reports Nav -->
+
+
+
+
+
+    <li class="nav-item">
+      <a class="nav-link navLinkJQ collapsed" href="pages-login.html">
+        <i class="bi bi-box-arrow-in-right"></i>
+        <span>Login</span>
+      </a>
+    </li><!-- End Login Page Nav -->
+
+
+  </ul>
+
+</aside>
